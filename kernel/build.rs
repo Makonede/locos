83a53101ba4512c8e88afb855c832f8643f4d738
JayAndJef <109392310+JayAndJef@users.noreply.github.com>
2025-04-19 18:45:32 -0700

    feat: continue migrating to limine
    remove bootimage crate, make custom compilation and layout for kernel binary

---
+++ kernel/build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // add linker and listener
+    println!("cargo:rerun-if-changed=linker.ld");
+    println!("cargo:rustc-link-arg=-Tlinker.ld");
+
+    // disable red zone
+    println!("cargo:rustc-link-arg=-mno-red-zone");
+}
