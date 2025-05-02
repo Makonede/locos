b8417f062e745e12ccac23f8f485d7fbb9963efb
JayAndJef <109392310+JayAndJef@users.noreply.github.com>
2025-05-01 18:55:38 -0700

    feat(output): logging macros
    moved macroes outside of `main` to `output`

---
+++ kernel/src/output/macros.rs
@@ -0,0 +1,70 @@
+//! Macros for printing to the framebuffer using the global terminal instance.
+
+/// Global print! macro that writes to the framebuffer.
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        {
+            use core::fmt::Write;
+            use $crate::output::FLANTERM;
+            let mut lock = FLANTERM.lock();
+            let writer = lock.as_mut().unwrap();
+            let _ = write!(writer, $($arg)*);
+        }
+    };
+}
+
+/// Logs an error message with a red "ERROR: " prefix.
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {
+        $crate::println!("\x1b[31mERROR:\x1b[0m {}", format_args!($($arg)*));
+    };
+}
+
+/// Logs a warning message with a yellow "WARN: " prefix.
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {
+        $crate::println!("\x1b[33mWARN:\x1b[0m {}", format_args!($($arg)*));
+    };
+}
+
+/// Logs an info message with a green "INFO: " prefix.
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {
+        $crate::println!("\x1b[32mINFO:\x1b[0m {}", format_args!($($arg)*));
+    };
+}
+
+/// Logs a debug message with a green "DEBUG: " prefix.
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        $crate::println!("\x1b[32mDEBUG:\x1b[0m {}", format_args!($($arg)*));
+    };
+}
+
+/// Logs a trace message with a light blue "TRACE: " prefix.
+#[macro_export]
+macro_rules! trace {
+    ($($arg:tt)*) => {
+        $crate::println!("\x1b[36mTRACE:\x1b[0m {}", format_args!($($arg)*));
+    };
+}
+
+/// Global println! macro that writes to the framebuffer.
+#[macro_export]
+macro_rules! println {
+    () => {
+        {
+            $crate::print!("\n");
+        }
+    };
+    ($($arg:tt)*) => {
+        {
+            $crate::print!("{}\n", format_args!($($arg)*));
+        }
+    };
+}
